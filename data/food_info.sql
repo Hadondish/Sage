78d3c0e9790b63750428f112950b6cf66bc112dc
Natalie Lunbeck <natalielunbeck@yahoo.com>
2024-04-20 23:51:31 +0000

    update test

--- data/food_info.sql
+++ data/food_info.sql
@@ -9,6 +9,4 @@ INSERT INTO products (name, description, price) VALUES
     ('image_003', 'Chicken of the Sea, Chunk Light Premium Tuna in Water, 7 oz, 12-Count', 19.99),
     ('image_005', 'Campbell''s, Simply Chicken Noodle Soup, 18.6 oz, 8-Count', 15.99),
     ('image_006', 'Rice Chex Cereal, 12 oz', 5.99),
-    ('image_007', 'Chick Tortilla Soup, 14.5 oz', 4.69),
-    ('image_008', 'Minestrone Soup, 15 oz', 4.99);
-
+    ('image_007', 'Chick Tortilla Soup, 14.5 oz', 4.69);
