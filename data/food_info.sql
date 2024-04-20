6f3ac4112a369d9b25f5cd787c246edc9d1bbf63
Natalie <natalielunbeck@yahoo.com>
2024-04-20 14:12:55 -0700

    add small db image_name --> info

---
+++ data/food_info.sql
@@ -0,0 +1,13 @@
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    description TEXT,
+    price REAL
+);
+
+INSERT INTO products (name, description, price) VALUES 
+    ('image_001', 'Kirkland Signature Chewy Protein Bar, Peanut Butter & Semisweet Chocolate Chip, 1.41 oz, 42-Count', 16.99),
+    ('image_002', 'Fruit By The Foot, Variety, 0.75 oz, 48-count', 19.99),
+    ('image_003', 'Chicken of the Sea, Chunk Light Premium Tuna in Water, 7 oz, 12-Count', 19.99),
+    ('image_004', 'Kirkland Signature, Almond Milk, 1 qt, 12-count', 14.99),
+    ('image_005', 'Campbell''s, Simply Chicken Noodle Soup, 18.6 oz, 8-Count', 15.99);
